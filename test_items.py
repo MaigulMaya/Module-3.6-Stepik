d2f0da0e3a1a2d589732820fbb639a12978da449
Maya <maigul.mukasheva@gmail.com>
2020-08-14 16:57:41 +0600

    Module 3

---
+++ test_items.py
@@ -0,0 +1,8 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_basket(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+    add_to_basket = browser.find_element_by_css_selector(".btn-add-to-basket")
+    browser.implicitly_wait(10)
+    assert add_to_basket.text == "Ajouter au panier", "Кнопка не найдена"
